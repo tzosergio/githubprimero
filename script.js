f392a7727c3de0b208fc6cd510efbc5b7c5a2970
Sergio tz <Sergio.serkebtonz@gmail.com>
2022-05-13 18:21:46 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+//document.write("<h1>hola mundo y a todos los youtuberos</h1>");
+var nombre = "Sergio tzompa";
+var altura = 190;
+
+document.write(nombre);
+document.write(altura);	
+
+
+for (var i = 0; i<=2020; i++) {
+	datos.innerHTML += "<h2>Estamos en el año: "+i;
+}
